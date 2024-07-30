26e05ca1123424401cbba7499aa55f266902bf04
Oleg <olegz11m@gmail.com>
2024-07-29 23:15:16 -0400

    index.js has been changed

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const {ApolloServer} = require('apollo-server');
-const mongoose = require('mongoose');
-const typeDefs = require("./graphql/typeDefs");
-const resolvers = require('./graphql/resolvers');
+const { ApolloServer } = require('apollo-server');
+const mongoose= require('mongoose')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 
-const MONGODB = 'mongodb+srv://Oleg:6249302@cluster0.ndt09ie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const server = new ApolloServer({
+const MONGODB = "mongodb+srv://Oleg:6249302@cluster0.ndt09ie.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const server =new ApolloServer( {
     typeDefs,
     resolvers
 })
 
-mongoose.connect(MONGODB, {useNewUrlParser: true})
-    .then(() => {
-        console.log("MongoDB Connection successful");
-        return server.listen({port: 5000})
-    })
-    .then((res) => {
-        console.log(`Server running at ${res.url}`);
-    })
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGODB);
+        console.log('MongoDB connected successfully');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+connectDB()
+server.listen({port: 5000}).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});

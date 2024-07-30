3abb90411685b1b4c1efac61f107911145b7ecf7
Oleg <olegz11m@gmail.com>
2024-07-30 00:03:44 -0400

    server ready

--- models/User.js
+++ models/User.js
@@ -1,23 +1,25 @@
-const mongoose = require('mongoose');
-const {model, Schema} = require('mongoose');
+const mongoose = require('mongoose')
+const {model, Schema} = require('mongoose')
 mongoose.Schema.Types.String.set('trim', true);
 
-const userSchema = new Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    firstName: {
-        type: String,
-        trim: true
-    },
-    lastName: {
-        type: String,
-        trim: true
-    },
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
-    ]
-})
+const userSchema = new Schema ({
+        _id: mongoose.Schema.Types.ObjectId,
 
-module.exports = model("User", userSchema);
+        firstName: {
+            type: String,
+        },
+
+        lastName: {
+            type: String,
+        },
+
+        comments: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Comment'
+            },
+        ]
+    }
+)
+
+module.exports = model('User', userSchema)
